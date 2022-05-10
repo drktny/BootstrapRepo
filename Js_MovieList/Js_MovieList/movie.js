0c75311abbace3bf81275dcd7c4c057365f3904e
Doruk <drktny@gmail.com>
2022-05-10 16:59:43 +0300

    Js_movielist js codes are addedç

---
+++ Js_MovieList/Js_MovieList/movie.js
@@ -0,0 +1,74 @@
+﻿class Movie {
+    constructor(id, movieName, director, category) {
+        this.id = id;
+        this.movieName = movieName;
+        this.director = director;
+        this.category = category;
+    }
+}
+
+//const movie = new Movie(1, "Spider-Man", "Sam Raimi", "Action");
+//console.log(movie);
+
+class WebUI {
+    static getMovies() {
+        const sampleMovies = [
+            {
+                id: 1,
+                movieName: "The Godfather",
+                director: "Francis Ford Cappola",
+                category: "Crime"
+            },
+            {
+                id: 2,
+                movieName: "The Lord of The Rings",
+                director: "Peter Jackson",
+                category: "Action"
+            },
+            {
+                id: 3,
+                movieName: "Interstellar",
+                director: "Christopher Nolan",
+                category: "Sci-Fi"
+            }
+        ]
+
+        for (var i = 0; i < sampleMovies.length; i++) {
+            WebUI.addMovie(sampleMovies[i]);
+        }
+    }
+
+    static addMovie(movie) {
+        const movieTable = document.getElementById("movieTable");
+        const tr = document.createElement("tr");
+
+        tr.innerHTML = `
+                        <td>${movie.id}</td>
+                        <td>${movie.movieName}</td>
+                        <td>${movie.director}</td>
+                        <td>${movie.category}</td>
+`
+        movieTable.appendChild(tr);
+    }
+    
+}
+
+window.onload = function () {
+    WebUI.getMovies();
+
+    document.getElementById('submit').onclick = function () {
+        const movieName = document.getElementById("movieName").value;
+        const director = document.getElementById("directorName").value;
+        const category = document.getElementById("categoryName").value;
+
+        const movie = new Movie(4, movieName, director, category);
+        console.log(movie);
+        WebUI.addMovie(movie);
+    }
+
+    
+}
+
+
+
+
---
+++ Js_MovieList/Js_MovieList/product.js
@@ -0,0 +1,44 @@
+﻿//objects
+const bread = { name: "wheat bread", price: 15 };
+const tea = { name: "chai", price: 45 };
+const rice = { name: "basmati", price: 30 };
+const chocolate = { name: "milka", price: 20 };
+
+//define Cart
+
+const cart = [];
+
+//add products to cart function
+
+function AddToCart() {
+    cart.push(bread);
+    cart.push(tea);
+    cart.push(rice);
+}
+
+//add and show on console
+AddToCart();
+console.log(cart);
+
+
+//{
+//var globalVariable = "a";
+//    let localVariable = "b";
+
+//}
+
+//console.log(globalVariable);
+
+
+//total price of products inside of cart
+
+function getCartTotalPrice() {
+    let totalPrice = 0;
+    for (var i = 0; i < cart.length; i++) {
+        totalPrice += cart[i].price;
+    }
+    console.log("Total price of your products is: " + totalPrice + " TL.");
+}
+
+getCartTotalPrice();
+
